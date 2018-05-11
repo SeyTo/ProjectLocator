11e8a19c311a0fc1d7eace7b11fd22535ec07968
rj <rajibbajracharya@gmail.com>
2018-05-11 13:40:28 +0545

    updated script to work

--- scripts/tales/windows-prepare.sh
+++ scripts/tales/windows-prepare.sh
@@ -1,17 +1,19 @@
 # opens all project of windows for i3 DE.
+#!/bin/zsh
 
 ROOT=/a/_proj/tales/
-i3-msg 'workspace 2; exec urxvtc -cd $ROOT/srcs/tales/src -e sh -c "vim ./App.vue"'
+
+i3-msg "workspace 2; exec urxvtc -cd $ROOT/srcs/tales/src -e sh -c 'vim ./App.vue'"
 # i3-msg 'workspace 2; exec code /a/_proj/tales/srcs/tales/src'
 # i3-msg 'workspace 3; exec urxvtc -cd /a/_proj/tales/srcs/tales -e sh -c npm start; exec urxvtc -cd /a/_proj/tales/srcs/tales; workspace 2'
-i3-msg '
+i3-msg "
   workspace 3; 
-  exec urxvt -cd $ROOT/srcs/tales -e sh -c "npm run dev";
+  exec urxvt -cd $ROOT/srcs/tales -e sh -c 'npm run dev';
   split v;
   exec urxvtc -cd $ROOT/srcs/tales/src
-  exec urxvtc -cd $ROOT/docs/ -e sh -c "vim todo"; 
-  workspace 2'
-sleep 20 &&
+  exec urxvtc -cd $ROOT/docs/ -e sh -c 'vim todo.md'; 
+  workspace 2"
+sleep 10 &&
 i3-msg 'exec firefox --new-tab localhost:8080;' && notify-send 'LOCALHOST window opened'
 
 
